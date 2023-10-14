a20a702c9b18075290ee4d787f7596e18d96e1c0
Wang Ben <wangben18@outlook.com>
2023-10-14 14:49:45 +0800

    update: from_into & using_as

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -40,10 +40,33 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of
 // Person Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
-
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        if s.len() == 0 {
+            return Person::default();
+        }
+        let mut s = s.split(",");
+        let name = match s.next() {
+            Some(n) => n,
+            None => return Person::default(),
+        };
+        if name.len() == 0 {
+            return Person::default();
+        }
+        let age = match s.next() {
+            Some(a) => match a.parse::<usize>() {
+                Ok(age_ok) => age_ok,
+                Err(_) => return Person::default(),
+            },
+            None => return Person::default(),
+        };
+        match s.next() {
+            Some(_) => Person::default(),
+            None => Person {
+                name: name.to_owned(),
+                age,
+            },
+        }
     }
 }
 
